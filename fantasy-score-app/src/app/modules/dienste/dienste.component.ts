8cef993103d61b63e8c1a9b80259ed0ce8936a23
Moumita <48108230+moumitahmad@users.noreply.github.com>
2020-06-05 12:10:23 +0200

    dienst

--- fantasy-score-app/src/app/modules/dienste/dienste.component.ts
+++ fantasy-score-app/src/app/modules/dienste/dienste.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DienstService } from 'src/app/services/dienst.service';
+import { Observable } from 'rxjs';
+import { Dienst } from 'src/app/models/Dienst';
 
 @Component({
   selector: 'app-dienste',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiensteComponent implements OnInit {
 
-  constructor() { }
+  kategorieID: number;
+  searchInput: string;
+
+  constructor(private dienstService: DienstService) { }
+
+  dienste: Observable<Dienst[]>;
+  shownDienste: Dienst[];
 
   ngOnInit(): void {
+    
+    this.dienste = this.dienstService.getDienste();
+    this.dienste.subscribe(data => {
+      console.log(data);
+      this.shownDienste = data;
+    });
+  }
+
+  onKategorieSelected(kategorieID):void {
+    if(kategorieID == "-1") { // Alle anzeigen
+      this.dienste = this.dienstService.getDienste();
+      this.dienste.subscribe(data => {
+        console.log(data);
+        this.shownDienste = data;
+      });
+      return;
+    }
+
+    this.dienste = this.dienstService.getDiensteInKategorie(kategorieID)
+    this.dienste.subscribe(data => {
+      this.shownDienste = data;
+    });
+  }
+
+  buchen(dienstID) {
+    console.log("Nutzer will den DIenst " + dienstID)+ "buchen";
   }
 
 }
